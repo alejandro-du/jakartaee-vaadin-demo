4c317cad010d69b1589aa5a4a981cc57a1361dd4
Alejandro Duarte <alejandro@vaadin.com>
2021-07-08 18:35:45 +0300

    Query method expressions

--- src/main/java/com/example/app/MainView.java
+++ src/main/java/com/example/app/MainView.java
@@ -1,10 +1,13 @@
 package com.example.app;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @Route("")
 public class MainView extends VerticalLayout {
@@ -13,6 +16,20 @@ public class MainView extends VerticalLayout {
     public MainView(UserRepository userRepository) {
         Long count = userRepository.count();
         Notification.show("Users: " + count);
+
+        Grid<User> grid = new Grid<>(User.class);
+        grid.setColumns("email", "birthDate");
+        grid.setItems(userRepository.findAll());
+
+        TextField filter = new TextField();
+        filter.setPlaceholder("Filter by email...");
+        filter.addValueChangeListener(event -> {
+            List<User> users = userRepository.findByEmailLike("%" + filter.getValue() + "%");
+            grid.setItems(users);
+        });
+
+
+        add(filter, grid);
     }
 
 }
--- src/main/java/com/example/app/UserRepository.java
+++ src/main/java/com/example/app/UserRepository.java
@@ -3,6 +3,11 @@ package com.example.app;
 import org.apache.deltaspike.data.api.EntityRepository;
 import org.apache.deltaspike.data.api.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends EntityRepository<User, Integer> {
+
+    List<User> findByEmailLike(String email);
+
 }
